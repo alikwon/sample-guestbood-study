66c39c95ea1a2de1a448135e110aaa623ca34e57
alikwon <63032589+alikwon@users.noreply.github.com>
2022-04-16 19:49:41 +0900

    페이지

--- src/main/java/org/alikwon/guestbook/controller/GuestBookController.java
+++ src/main/java/org/alikwon/guestbook/controller/GuestBookController.java
@@ -1,17 +1,30 @@
 package org.alikwon.guestbook.controller;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.alikwon.guestbook.dto.PageRequestDTO;
+import org.alikwon.guestbook.service.GuestbookService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/guestbook")
 @Log4j2
+@RequiredArgsConstructor
 public class GuestBookController {
-    @GetMapping({"/","","/list"})
+
+    private final GuestbookService service;
+
+    @GetMapping({"/",""})
     public String list(){
-       log.info("########## list ##########");
-       return "/guestbook/list";
+       return "redirect:/guestbook/list";
+    }
+
+    @GetMapping("/list")
+    public void list(PageRequestDTO pageRequestDTO, Model model) {
+        log.info("########## list ##########");
+        model.addAttribute("result", service.getList(pageRequestDTO));
     }
 }
